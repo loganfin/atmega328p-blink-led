2614d10adf838e47e0084de62ee08ab85dd87c7d
loganfin <finley4.logan@gmail.com>
2023-01-06 10:12:24 -0800

    More concise cmake build

--- src/main.c
+++ src/main.c
@@ -3,11 +3,18 @@
 #include <util/delay.h>
 #include <stdbool.h>
 
-#define BLINK_MS 1000
+#define BLINK_MS 500
 
 int main()
 {
     DDRB |= 0b00100000;
+
+    /*
+    PORTB |= 0b00100000;
+    _delay_ms(BLINK_MS);
+    PORTB &= ~0b00100000;
+    _delay_ms(BLINK_MS);
+    */
 
     while (true) {
         PORTB |= 0b00100000;
